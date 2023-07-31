690ca80719375e4a9a17983e8ad49fdb21d3053d
sarehnaderi <140264213+sarehnaderi@users.noreply.github.com>
2023-07-31 22:56:01 +0330

    Update Assignment 3.py

--- Assignment 3.py
+++ Assignment 3.py
@@ -12,7 +12,7 @@ if start == 1:
     print("Result: ",x)
 if x == 6:
     award_1 = random.choice([1,2,3,4,5,6])
-    print("Congratulation! You got 6! Here is your award roll:" )
+    print("Congratulation! You got 6! Here is your award roll:",award_1 )
     if award_1 == 6:
         award_2 = random.choice([1,2,3,4,5,6])
         print("Wow you got 6 again! Here is your second award roll:",award_2)
